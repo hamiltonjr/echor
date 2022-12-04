22fff7d6d21e3bc91f02dae91ed7e5d2aa6f7f4f
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2022-12-04 14:55:48 -0300

    Including clap crate and other changes.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let _matches = App::new("echor")
+        .version("0.1.0")
+        .author("Hamilton G. Jr. <hamiltonjr2010@gmail.com>")
+        .about("Rust echo")
+        .get_matches();
 }
