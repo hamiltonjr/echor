8b6dbfe7b548ec8c00a080afcf129f1ca55c635a
Hamilton Gonçalves Junior <hamiltonjr2010@gmail.com>
2022-12-04 16:11:25 -0300

    Coding all the handlings.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("echor")
+    // flags handling using clap
+    let matches = App::new("echor")
         .version("0.1.0")
         .author("Hamilton G. Jr. <hamiltonjr2010@gmail.com>")
         .about("Rust echo")
-        .get_matches();
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+    .get_matches();
+
+    // text handling
+    let text = matches.values_of_lossy("text").unwrap();
+    
+    // newline handling
+    let omit_newline = matches.is_present("omit_newline");
+    
+    // output
+    print!("{}{}", text.join(" "), 
+        if omit_newline { "" } else { "\n" });
+
 }
